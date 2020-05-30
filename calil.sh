2994cb3a6ab62ab729ceb7cffef551c5c4758467
liuqw <liuqingwei@ainnovation.com>
2020-05-30 12:19:16 +0800

    refactor: local adapter.

--- calil.sh
+++ calil.sh
@@ -9,18 +9,13 @@ do
 	(cd $i;git pull origin master)
 done
 
-rm -rf env
-virtualenv env
-source env/bin/activate
-(cd genpac;python setup.py install)
-
-env/bin/genpac \
-	--pac-proxy "SOCKS5 127.0.0.1:1080" \
+genpac \
+	--pac-proxy "SOCKS5 127.0.0.1:8387" \
 	--gfwlist-url - \
 	--gfwlist-local gfwlist/gfwlist.txt \
+	--user-rule-from user_rule.txt \
 	-o gfwlist.pac
-sed -e '5d' -e '3d' -i gfwlist.pac
-deactivate
+# sed -e '5d' -e '3d' -i gfwlist.pac
 
 git add .
 git commit -m "[$(LANG=C date)]auto update"
